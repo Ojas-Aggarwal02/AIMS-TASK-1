d775495669d6addce912cb77800061f92ebfaf5e
Ojas-Aggarwal02 <ojasaggarwal12@gmail.com>
2025-10-23 23:41:57 +0530

    Refactor ordinal encoding to use user-defined mapping

--- ordinal-encoding.py
+++ ordinal-encoding.py
@@ -1,21 +1,24 @@
 import pandas as pd
 import numpy as np
 
-
 data = {
-    'Age': [22, 25, 28, 24, 30, 35, 40, 26, 31, 27],  # Non-categorical
-    'Salary': [30000, 35000, 40000, 32000, 45000, 50000, 55000, 37000, 48000, 39000],  # Non-categorical
-    'Department': ['HR', 'IT', 'Sales', 'HR', 'Sales', 'IT', 'HR', 'IT', 'Sales', 'HR'],  # Nominal (categorical, unordered)
-    'Education': ['High School', 'Bachelors', 'Masters', 'PhD', 'Bachelors', 'Masters', 'PhD', 'Masters', 'High School', 'Bachelors'] 
-    }
+    'Age': [22, 25, 28, 24, 30, 35, 40, 26, 31, 27],
+    'Salary': [30000, 35000, 40000, 32000, 45000, 50000, 55000, 37000, 48000, 39000],
+    'Department': ['HR', 'IT', 'Sales', 'HR', 'Sales', 'IT', 'HR', 'IT', 'Sales', 'HR'],
+    'Education': ['High School', 'Bachelors', 'Masters', 'PhD', 'Bachelors', 'Masters', 'PhD', 'Masters', 'High School', 'Bachelors']
+}
 
 df = pd.DataFrame(data)
-
+dictionary = {}  
 print("Original DataFrame:\n", df)
 
+for education in df['Education'].unique():
+    number = float(input(f"Enter number u want to assign for {education}: "))
+    
+    dictionary[education] = number
+
 df.replace({
-    'Education': { 'High School': 1, 'Bachelors': 2, 'Masters': 3, 'PhD': 4 }
+    'Education': dictionary
 }, inplace=True)
 
-print("\nDataFrame after Ordinal Encoding:\n", df)
-
+print("\nDataFrame after Ordinal Encoding:\n",df)
